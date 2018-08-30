89ccf2bc4a2b98b4236305f3d0912c57019d56a5
pl3nny <pl3nny@gmail.com>
2018-08-30 09:58:08 -0700

    created UI class, modified roulette class to include new method to exchange player money to chips...still workingo on that method

--- roulette/src/javatar/game/Roulette.java
+++ roulette/src/javatar/game/Roulette.java
@@ -4,17 +4,37 @@ import java.util.Scanner;
 
 public class Roulette 
 {
+
+	Scanner scan = new Scanner(System.in);
+
 	private String gameVersion;
     private int numOfPlayers;
-    private double houseMoney;
+    private double houseMoney = 100000;
     private int houseChips;
+
+    // game version 1
+    // single zero roulette
+    // 0-32-15-19-4-21-2-25-17-34-6-27-13-36-11-30-8-23-10-5-24-16-
+    //33-1-20-14-31-9-22-18-29-7-28-12-35-3-26
+
+    private int singleZero[] = {0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6,
+     27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 2, 18, 29, 7, 28,
+      12, 28, 12, 35, 3, 26};
+
+   	// game version 2
+    // double zero roulette   ---> -2 for double zero (00)
+    // 0-28-9-26-30-11-7-20-32-17-5-22-34-15-3-24-36-13-1-00-27-10-25
+    //-29-12-8-19-31-18-6-21-33-16-4-23-35-14-2
+
+    private int doubleZero[] = {0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5,
+     22, 34, 15, 3, 24, 36, 13, 1, -2, 27, 10, 25, 29, 12, 8, 1, 9, 31,
+      18, 6, 21, 33, 16, 4, 23, 35, 14, 2};
+
 
     Roulette(){};
     
     Roulette(String gameVersion, int numOfPlayers)
     {
-    	Scanner scan = new Scanner(System.in);
-
     	this.gameVersion = gameVersion;
     	this.numOfPlayers = numOfPlayers;
     }
@@ -62,21 +82,45 @@ public class Roulette
     public int buyChips(double money)
     {
     	int selection;
+    	double moneyToChips = 0;
 
     	if(gameVersion.equals("100A"))
     	{
-    		switch(selection)
+    		while(money > 1)
     		{
-    			
+    			selection = scan.nextInt();
+	    		switch(selection)
+	    		{
+	    			case 1:
+	    				break;
+	    			case 20:
+	    				break;
+	    			case 50:
+	    				break;
+	    			case 100:
+	    				break;
+	    			case 200:
+	    				break;
+	    			case 1000:
+	    				break;
+
+	    		}
     		}
+
+    		return houseChips;
     	}
     	else if(gameVersion.equals("100B"))
     	{
-
+    		return houseChips;
     	}
     	else
     	{
     		return -1;
     	}
+    }
+
+    public void spinWheel()
+    {
+
     }
 }
---
+++ roulette/src/javatar/game/UI.java
@@ -0,0 +1,11 @@
+package javatar.game;
+
+public class UI 
+{
+	public UI(){};
+
+	public void buyChipsUI()
+	{
+		System.out.print("Enter amount of money you wish to exchange for chips: ");
+	}
+}
