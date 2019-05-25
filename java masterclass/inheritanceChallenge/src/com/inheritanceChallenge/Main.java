f0c984d41670d0c624e86ff8a9016977e76cb0a8
pl3nny <pl3nny@gmail.com>
2019-05-25 10:49:49 -0700

    starting in inheritanceChallenge

---
+++ java masterclass/inheritanceChallenge/src/com/inheritanceChallenge/Main.java
@@ -0,0 +1,20 @@
+package com.inheritanceChallenge;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        /*
+        * Challenge
+        * Start with a base class of a vehicle, then create a Car class tht inherits from this base class
+        * Finally, create another class, a specific type of Car that inherits from the Car class.
+        * You should be able to hand steer, change gears, move(speed)
+        * You will want to decide where to put the appropriate state and behaviours (fields and methods)
+        * As mentioned above, changing gears, increasing/decreasing speed should be included
+        * For you specific type of vehicle you want to add something specific for that ype of car
+        */
+
+        System.out.println("Smoke Tires Not Drugs");
+    }
+}
