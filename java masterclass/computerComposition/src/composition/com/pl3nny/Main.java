5d1aa7d7354d3458b06bed30b2aaaf965cfb393c
pl3nny <pl3nny@gmail.com>
2019-06-04 09:57:58 -0700

    PC composition

---
+++ java masterclass/computerComposition/src/composition/com/pl3nny/Main.java
@@ -0,0 +1,11 @@
+package composition.com.pl3nny;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	// write your code here
+        System.out.println("Alexander Hernandez");
+
+    }
+}
