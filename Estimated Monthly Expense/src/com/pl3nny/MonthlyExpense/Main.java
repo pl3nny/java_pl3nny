3b847fd2c1d0040d07d28b9ab32259e66e88735e
pl3nny <pl3nny@gmail.com>
2019-06-15 22:26:55 -0700

    work on monthly expense options

--- Estimated Monthly Expense/src/com/pl3nny/MonthlyExpense/Main.java
+++ Estimated Monthly Expense/src/com/pl3nny/MonthlyExpense/Main.java
@@ -1,10 +1,12 @@
 package com.pl3nny.MonthlyExpense;
 
-
 public class Main {
 
     public static void main(String[] args)
     {
+        System.out.println("Author: Alexander Hernandez\n");
+        System.out.println("\tMonthly Expense Program\n");
+
 
     }
 }
